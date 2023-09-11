cea32f444e95e6b352305a28f24d83c2055ded14
JH Lee <13332150+oulico@users.noreply.github.com>
2023-09-11 11:32:22 +0900

    FIX: moved DSN key

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,11 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://eff294d0f99f4d808f85f54558cbf67f@o4505556067483648.ingest.sentry.io/4505556079738880",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 });
